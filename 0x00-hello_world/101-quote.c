9a7e2a9c98897314bef4c4c2e5edb8749aa45a94
osm-pixle <ousamaeddahbir@gmail.com>
2023-02-16 20:08:10 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - Entry
- * Return: Always 0
+ * Return: 1
  */
 int mani(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	return (1);
 }
 
