e8337eb68cdf1e0e48e1dda393ea5ed8fe252b27
osm-pixle <ousamaeddahbir@gmail.com>
2023-02-16 15:05:51 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main -Entry point
+ *Return: Always 0 (Souccess)
+ */
+
+int main() {
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
+}
+
