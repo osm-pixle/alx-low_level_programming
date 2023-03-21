6825b4e9199070898462f491b8113483fa59e721
osm-pixle <ousamaeddahbir@gmail.com>
2023-03-21 12:59:32 -0700

    hi

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
-
 /**
- * _islower - Checks for lowercase character
+ * _isalpha - Checks for alphabetic character
  * @c: The character to be checked
- * Return: 1 for lowercase character or 0 for anything else
+ * Return: 1 for alphabetic character or 0 for anything else
  */
 
-int _islower(int c)
+int _isalpha(int c)
 
 {
 
-	if (c >= 97 && c <= 122)
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
-
 	return (1);
 	}
 	return (0);
--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -2,19 +2,19 @@
 
 /**
  * main - check the code.
- *Return: Always 0.
+ * Return: Always 0.
  */
 
 int main(void)
 
 {
 
-    	int r;
-    
+	int r;
+      
 	r = _isalpha('H');
         _putchar(r + '0');
         r = _isalpha('o');
-    	_putchar(r + '0');
+        _putchar(r + '0');
         r = _isalpha(108);
         _putchar(r + '0');
         r = _isalpha(';');
